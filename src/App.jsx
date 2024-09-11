443646ca5bea6db491e53d029a86719f3f05a6a7
bojtarkrisztian <bojtar.krisztian@appsint.hu>
2024-09-11 17:52:14 +0200

    separeted components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import React from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
+const INIT_INPUTS = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
 function App() {
+  const [userInput, setUserInput] = React.useState(INIT_INPUTS);
+
+  const handleUserInputChange = (event, inputLabel) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputLabel]: +event.target.value,
+      };
+    });
+  };
+
+  const inputIsValid = userInput.duration >= 1;
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleUserInputChange} />
+      {
+        inputIsValid 
+        ? <Results input={userInput} />
+        :<p className="center">Please enter a duration greater than zero</p>
+      }
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UserInput.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+
+const UserInput = ({userInput, onChange}) => {
+  
+
+  
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChange(event, "initialInvestment")
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChange(event, "annualInvestment")
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              onChange(event, "expectedReturn")
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              onChange(event, "duration")
+            }
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
